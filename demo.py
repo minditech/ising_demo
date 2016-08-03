3229786fac13c47147dd57ca5dc2695acc21cca1
Ethan Brown <ethan.w.brown@gmail.com>
2016-08-03 09:23:30 -0400

    updated to +/- 1

--- demo.py
+++ demo.py
@@ -1,30 +1,30 @@
 #! /usr/bin/env python
 import sys, random, numpy
 
-# Evaluate the energy given a Hamiltonian and configuration
-def evaluate_energy(H, cfg):
+# Evaluate the energy given a coupling matrix and configuration
+def evaluate_energy(J, cfg):
     tot = 0
     for i in range(len(cfg)-1):
         for j in range(len(cfg)):
-            tot += H[i,j]*((-1)**(cfg[i]+cfg[j]))
+            tot += J[i,j]*cfg[i]*cfg[j]
     return tot
 
-# Read Hamiltonian
+# Read coupling matrix
 N = 300
-H = numpy.zeros((N,N))
+J = numpy.zeros((N,N))
 with open(sys.argv[1], 'r') as f:
     for line in f:
         line = line.split()
         spin_a, spin_b, coupling = int(line[0]), int(line[1]), float(line[2])
-        H[spin_a,spin_b] = coupling
-        H[spin_b,spin_a] = coupling
+        J[spin_a,spin_b] = coupling
+        J[spin_b,spin_a] = coupling
 
-# Solve Hamiltonian for minimum spin configuration (by random guessing)
+# Solve for minimum spin configuration (by random guessing)
 min_energy = 1.e9
-min_cfg = [0]*N
+min_cfg = [1]*N
 for i in range(10):
-    cfg = [int(2*random.random()) for i in xrange(N)]
-    energy = evaluate_energy(H, cfg)
+    cfg = [(2*random.randint(0,1)-1) for i in xrange(N)]
+    energy = evaluate_energy(J, cfg)
     if energy < min_energy:
         min_energy = energy
         min_cfg = cfg
